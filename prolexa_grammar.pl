f2e72854904737818530aa59a6a379073f0101b2
Jack Hanslope <code@jackhanslope.com>
2020-12-20 11:04:04 +0000

    Add handling of "can"

--- prolexa_grammar.pl
+++ prolexa_grammar.pl
@@ -84,6 +84,7 @@ qword --> [].
 question1(Q) --> [who],verb_phrase(s,_X=>Q).
 question1(Q) --> [is], proper_noun(N,X),property(N,X=>Q).
 question1(Q) --> [does],proper_noun(_,X),verb_phrase(_,X=>Q).
+question1(Q) --> [can],proper_noun(_,X),verb_phrase(_,X=>Q).
 %question1((Q1,Q2)) --> [are,some],noun(p,sk=>Q1),
 %					  property(p,sk=>Q2).
 
